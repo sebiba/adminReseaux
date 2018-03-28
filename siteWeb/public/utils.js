86f2ed1a3e590704018e093b876c3db155d24618
Bendmh <32776475+Bendmh@users.noreply.github.com>
2018-03-28 21:55:27 +0200

    Add files via upload

---
+++ siteWeb/public/utils.js
@@ -0,0 +1,16 @@
+﻿// librairie de fonctions utiles
+// ChL octobre 2014
+
+// fonctions utilitaires
+function setElem(id, v){
+  // place la valeur v dans l'élément id
+  document.getElementById(id).innerHTML = v;
+}
+function addElem(id, v){
+  // ajoute la valeur v au contenu de l'élément id
+  document.getElementById(id).innerHTML += v;
+}   
+/*function getElem(id){
+  // renvoie le contenu de l'élément id
+  return document.getElementById(id).innerHTML;
+} */
